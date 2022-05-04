d9518c7c64abdee941edfe59b288827412403d84
라천호 수석 <ing@feelanet.com>
2022-05-04 13:44:59 +0900

    Boilerplate Web Service 세팅

---
+++ Boilerplate-Service/Boilerplate.Service/Program.cs
@@ -0,0 +1,72 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Boilerplate.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Topshelf;
+using Host = Microsoft.Extensions.Hosting.Host;
+
+namespace Boilerplate.Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<IHost>(s =>
+                {
+                    s.ConstructUsing(() => CreateHostBuilder(args).Build());
+                    s.WhenStarted(service =>
+                    {
+                        service.Start();
+                    });
+
+                    s.WhenStopped(service =>
+                    {
+                        service.StopAsync();
+                    });
+                });
+
+                x.StartAutomatically();
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("Boilerplate.Service.ServiceName");
+                x.SetDisplayName("Boilerplate.Service.DisplayName");
+                x.SetDescription("Boilerplate.Service.Description");
+            });
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+#if DEBUG
+                 .UseEnvironment(Environments.Development)
+#else
+                .UseEnvironment(Environments.Production)
+                //.UseEnvironment(Environments.Staging)
+#endif
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("appsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                //.UseSerilog()
+                .ConfigureLogging((context, logger) =>
+                {
+                    logger.AddConsole();
+                    //logger.AddLog4Net();
+                    logger.SetMinimumLevel(LogLevel.Information);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
---
+++ Boilerplate-Service/Boilerplate.Web/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+#if DEBUG
+                 .UseEnvironment(Environments.Development)
+#else
+                .UseEnvironment(Environments.Production)
+                //.UseEnvironment(Environments.Staging)
+#endif
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                //.UseSerilog()
+                .ConfigureLogging((context, logger) =>
+                {
+                    logger.AddConsole();
+                    //logger.AddLog4Net();
+                    logger.SetMinimumLevel(LogLevel.Information);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
